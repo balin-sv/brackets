c212f96ff029ba1aa8cc727b3c134a058777ece2
balin-sv <balinsvet@gmail.com>
2021-08-30 21:38:47 +0400

    add all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let current;
+  let opens = [];
+  let closes = {};
+  let stack = [];
+  for (let o = 0; o < bracketsConfig.length; o++) {
+    for (let r = 0; r < bracketsConfig[o].length; r++) {
+      if (r === 0) {
+        opens.push(bracketsConfig[o][r]);
+      } else {
+        closes[bracketsConfig[o][r]] = bracketsConfig[o][r - 1];
+      }
+    }
+  }
+  console.log(opens);
+  console.log(closes);
+
+  for (let o = 0; o < str.length; o++) {
+    current = str[o];
+    console.log(current);
+
+    if (opens.includes(current)) {
+      stack.push(current);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let top = stack[stack.length - 1];
+
+      if (closes[current] === top) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
